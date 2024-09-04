50417af0e50d63e9ed960a5f16040d261904513c
Tembwe <temcoding600@gmail.com>
2024-09-04 10:13:01 +0100

    pushing project work

---
+++ js/app.js
@@ -0,0 +1,118 @@
+console.log('Quiz game!');
+
+/*-------------- Constants -------------*/
+
+const question = [
+    {
+        questio: "What English Premier League club plays at a stadium known as the Theatre of Dreams?",
+        option: ["Brentford fc", "Manchester United", "Manchester City", "Arsenal"],
+        answer: "Manchester United"
+    },
+
+    {
+        questio: "What position did NFL great Rob Gronkowski play?",
+        option: ["offensive lineman", "running back", "", "linebacker"],
+        answer: ""
+    },
+
+    {
+        questio: "How many times have Brazil won the World Cup?",
+        option: ["5", "1", "6", "8"],
+        answer: "5"
+    },
+
+    {
+        questio: "Which Golfer is credited with inventing the ‘Stinger’ shot?",
+        option: ["Scottie Scheffler", "Jake Knapp", "Rory McIlroy", "Tiger Woods"],
+        answer: "Tiger Woods"
+    },
+
+    {
+        questio: "Who was formerly known as Cassius Clay?",
+        option: ["Muhammad Ali", "Mike Tyson", "Jack Johnson", "Manny Pacquiao"],
+        answer: "Muhammad Ali"
+    },
+
+    {
+        questio: "In what year was the first World Cup?",
+        option: ["1845", "2010", "1989", "1930"],
+        answer: "1930"
+    },
+
+    {
+        questio: "What is Elton John’s real name?",
+        option: ["John Cash", "Reginald Kenneth Dwight", "Elvis Presley", "Peter Whight"],
+        answer: "Reginald Kenneth Dwight"
+    },
+
+    {
+        questio: "How many teams are there in Formula 1?",
+        option: ["30", "40", "10", "20"],
+        answer: "10"
+    },
+
+    {
+        questio: "Before becoming famous, where did Harry Styles work?",
+        option: ["At a Food Restaurant", "At JP Morgan", "At a Car Factory",  "At a bakery"],
+        answer: "At a bakery"
+    },
+
+    {
+        questio: "What rock guitarist played on the Michael Jackson song “Beat It?",
+        option: ["Eddie Van Halen", "Brian May", "Jeff Beck", "Jimi Hendrix"],
+        answer: "Eddie Van Halenn"
+    },
+
+    {
+        questio: "Angus Young is co-founder and lead guitarist for what Australian band?",
+        option: ["Alchetron", "AC/DC", "The Wanted", "Ultimate Boy bands"],
+        answer: "AC/DC"
+    },
+
+    {
+        questio: "What is Taylor Swift’s middle name?",
+        option: ["Underwood", "Hannah", "Natalie", "Alison"],
+        answer: "Alison"
+    },
+
+    {
+        questio: "Eminem stars and performs in what semi-autobiographical movie from 2002?",
+        option: ["The Sea Inside", "8 Mile", "Remember The Titans", "The Pianist"],
+        answer: "8 Mile"
+    },
+
+]
+
+/*---------- Variables (state) ---------*/
+let score = 0;
+let userAnswers = [];
+let questions = [];
+let currentquestionIndex = 0;
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+function startGame() {
+    
+}
+console.log(startGame);
+
+
+function showQuestion() {
+
+}
+console.log(showQuestion);
+
+function endGame() {
+
+}
+
+
+
+
+
+
+startGame();
+/*----------- Event Listeners ----------*/
+
